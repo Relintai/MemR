f2814b332dbdc3e06ef0af694512022ea4b9ed95
Relintai <relintai@protonmail.com>
2022-12-11 11:46:31 +0100

    Implemented shrink override setting.

--- game/Main.gd
+++ game/Main.gd
@@ -10,6 +10,13 @@ func hide_all() -> void:
 	_settings.hide()
 	_sort.hide()
 
+func apply_shink() -> void:
+	if !ProjectSettings.get("display/window/stretch/shrink_override"):
+		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_DISABLED, SceneTree.STRETCH_ASPECT_IGNORE, Vector2(900, 1000), clamp(OS.get_screen_dpi() / 72.0, 1, 3))
+	else:
+		var shrink_override_value : float = ProjectSettings.get("display/window/stretch/shrink_override_value")
+		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_DISABLED, SceneTree.STRETCH_ASPECT_IGNORE, Vector2(900, 1000), shrink_override_value)
+		
 func _on_Sort_pressed() -> void:
 	if !_sort.validate_folders():
 		_folder_setup_error_popup.popup_centered()
@@ -24,8 +31,6 @@ func _on_Settings_pressed() -> void:
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_READY:
-		get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_DISABLED, SceneTree.STRETCH_ASPECT_IGNORE, Vector2(900, 1000), clamp(OS.get_screen_dpi() / 72.0, 1, 3))
-		
 		_menu = get_node("Menu")
 		_settings = get_node("Settings")
 		_sort = get_node("Sort")
--- game/settings/Settings.gd
+++ game/settings/Settings.gd
@@ -6,6 +6,8 @@ signal back
 
 var _sort_folder_line_edit : LineEdit
 var _target_folder_line_edit : LineEdit
+var _shrink_override_cb : CheckButton
+var _shrink_override_value_slider : HSlider
 
 func load_settings() -> void:
 	var config_file : ConfigFile = ConfigFile.new()
@@ -18,12 +20,20 @@ func load_settings() -> void:
 	
 	var sort_folder : String = config_file.get_value("default", "sort_folder", "")
 	var target_folder : String = config_file.get_value("default", "target_folder", "")
+	var shrink_override : bool = config_file.get_value("default", "shrink_override", false)
+	var shrink_override_value : float = config_file.get_value("default", "shrink_override_value", 1)
 	
 	ProjectSettings.set("application/config/sort_folder", sort_folder)
 	ProjectSettings.set("application/config/target_folder", target_folder)
+	ProjectSettings.set("display/window/stretch/shrink_override", shrink_override)
+	ProjectSettings.set("display/window/stretch/shrink_override_value", shrink_override_value)
 	
 	_sort_folder_line_edit.text = sort_folder
 	_target_folder_line_edit.text = target_folder
+	_shrink_override_cb.pressed = shrink_override
+	_shrink_override_value_slider.value = shrink_override_value
+	
+	get_parent().apply_shink()
 	
 	
 func save_settings() -> void:
@@ -31,18 +41,26 @@ func save_settings() -> void:
 	
 	var sort_folder : String = _sort_folder_line_edit.text
 	var target_folder : String = _target_folder_line_edit.text
+	var shrink_override : bool = _shrink_override_cb.pressed
+	var shrink_override_value : float = _shrink_override_value_slider.value
 	
 	config_file.set_value("default", "sort_folder", sort_folder)
 	config_file.set_value("default", "target_folder", target_folder)
-	
+	config_file.set_value("default", "shrink_override", shrink_override)
+	config_file.set_value("default", "shrink_override_value", shrink_override_value)
+
 	ProjectSettings.set("application/config/sort_folder", sort_folder)
 	ProjectSettings.set("application/config/target_folder", target_folder)
+	ProjectSettings.set("display/window/stretch/shrink_override", shrink_override)
+	ProjectSettings.set("display/window/stretch/shrink_override_value", shrink_override_value)
 	
 	var error : int = config_file.save(settings_file)
 	
 	if error != OK:
 #		print("Failed to load the settings file! Error code %s" % error)
 		return
+		
+	get_parent().apply_shink()
 
 func _on_Button_pressed() -> void:
 	save_settings()
@@ -52,4 +70,7 @@ func _notification(what: int) -> void:
 	if what == NOTIFICATION_READY:
 		_sort_folder_line_edit = get_node("VBoxContainer/SortFolder") as LineEdit
 		_target_folder_line_edit = get_node("VBoxContainer/TargetFolder") as LineEdit
+		_shrink_override_cb = get_node("VBoxContainer/ShrinkOverride") as CheckButton
+		_shrink_override_value_slider = get_node("VBoxContainer/ShrinkOverrideValue") as HSlider
+
 		load_settings()
